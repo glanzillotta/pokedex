bdeb2589f1406fa4fc09c52c1f5d0a5585690a53
g-lanzillotta <glanzillotta@cybsec.it>
2024-07-11 15:50:00 +0200

    + home page and react query provider

---
+++ pages/HomePage.tsx
@@ -0,0 +1,70 @@
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {ListCard, SearchBar, Title} from '../components';
+import React from 'react';
+
+function HomePage() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.white,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Title>Pokedex</Title>
+          <Text style={styles.subTitle}>
+            Cerca un pokemo tramite nome o numero del pokedex
+          </Text>
+          <SearchBar />
+        </View>
+      </ScrollView>
+      <ListCard />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  subTitle: {
+    fontSize: 18,
+    fontFamily: 'Nunito-Regualr',
+    color: '#333',
+  },
+});
+
+export default HomePage;
