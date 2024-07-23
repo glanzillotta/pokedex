3d89aa82de487d7f66eebb838ef9cac97aa97d9b
Giuseppe Lanzillotta <glanzillotta@cybsec.it>
2024-07-23 12:04:59 +0200

    add empty page text

--- screen/FavouriteScreen.tsx
+++ screen/FavouriteScreen.tsx
@@ -3,21 +3,32 @@ import {
   ListRenderItemInfo,
   SafeAreaView,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
 import React from 'react';
 import {RootState} from '../store/store.ts';
 import useAppSelector from '../hooks/useSelector.ts';
-import {PokemonCard} from '../components';
+import {PokemonCard, Title} from '../components';
 import {Pokemon} from '../store/favouriteSlice.ts';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function FavouriteScreen() {
   const pokemon = useAppSelector(
-    (state: RootState) => state.pokemon.pokemonList,
+    (state: RootState) => state.favourite.pokemonList,
   );
   return (
     <SafeAreaView style={styles.pageContainer}>
       <View>
+        <Title>Favourite</Title>
+        {!Array.isArray(pokemon) || !pokemon.length ? (
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>
+              Non ci sono pokemon tra i preferiti.
+            </Text>
+            <Icon name="heart-dislike-outline" size={50} color="#333" />
+          </View>
+        ) : null}
         <FlatList
           data={pokemon}
           renderItem={renderItem}
@@ -39,6 +50,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: '100%',
     width: '100%',
+    paddingTop: 10,
+  },
+  emptyContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+  emptyText: {
+    color: '#333',
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
 
