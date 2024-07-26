f54742e47a38faf6847a53858121314353bf787d
Giuseppe Lanzillotta <glanzillotta@cybsec.it>
2024-07-26 11:35:52 +0200

    update card and pokemon detail with gradient or colo background component

--- components/Cards/PokemonCard.tsx
+++ components/Cards/PokemonCard.tsx
@@ -1,6 +1,5 @@
 import React, {memo} from 'react';
 import {useQuery} from '@tanstack/react-query';
-import Card from './Card.tsx';
 import {
   addPokemon,
   Pokemon,
@@ -10,7 +9,9 @@ import useDispatch from '../../hooks/useDispatch.ts';
 import useAppSelector from '../../hooks/useSelector.ts';
 import {RootState} from '../../store/store.ts';
 import {getBackgroundColor, PokemonType} from '../../utils/colors.ts';
-import {GestureResponderEvent, Pressable} from 'react-native';
+import {GestureResponderEvent, Pressable, StyleSheet} from 'react-native';
+import GradientOrColorContainer from '../GradientOrColorContainer.tsx';
+import CardContent from './CardContent.tsx';
 
 interface PokemonCardProps {
   pokemon: Pokemon;
@@ -19,6 +20,13 @@ interface PokemonCardProps {
 
 interface Response {
   types: PokemonType[];
+  sprites: {
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
 }
 
 const PokemonCard = ({pokemon, onPressCard}: PokemonCardProps) => {
@@ -49,16 +57,34 @@ const PokemonCard = ({pokemon, onPressCard}: PokemonCardProps) => {
 
   return (
     <Pressable onPress={onPressCard}>
-      <Card
-        id={id}
-        name={pokemon.name}
-        imageUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-        onPressFavourite={handelFavourite}
-        isFavourite={!!pokemonList.find(p => p.name === pokemon.name)}
+      <GradientOrColorContainer
         backgroundColor={getBackgroundColor(data?.types || [])}
-      />
+        style={styles.card}>
+        <CardContent
+          id={id}
+          name={pokemon.name}
+          imageUrl={data?.sprites.other['official-artwork']?.front_default}
+          onPressFavourite={handelFavourite}
+          isFavourite={!!pokemonList.find(p => p.name === pokemon.name)}
+        />
+      </GradientOrColorContainer>
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    alignItems: 'center',
+    padding: 20,
+    margin: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    height: 195,
+    width: 160,
+  },
+});
 
 export default memo(PokemonCard);
--- screen/DetailPokemonScreen.tsx
+++ screen/DetailPokemonScreen.tsx
@@ -1,11 +1,54 @@
-import {Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react';
+import {DetailPokemonNavigationProps} from '../navigation';
+import {useQuery} from '@tanstack/react-query';
+import {getBackgroundColor} from '../utils/colors.ts';
+import {GradientOrColorContainer} from '../components';
 
-function DetailPokemonScreen() {
+interface DetailPokemonScreenProps extends DetailPokemonNavigationProps {}
+
+function DetailPokemonScreen({route}: DetailPokemonScreenProps) {
+  const {data, isSuccess} = useQuery({
+    queryKey: ['pokemon', route.params.name],
+    queryFn: async () => {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${route.params.name}`,
+      );
+      return response.json();
+    },
+  });
+
   return (
-    <View>
-      <Text>Detail Pokemon Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <GradientOrColorContainer
+        backgroundColor={getBackgroundColor(data?.types || [])}
+        style={styles.imageContainer}>
+        {isSuccess && (
+          <Image
+            width={200}
+            height={200}
+            source={{
+              uri: data?.sprites.other['official-artwork']?.front_default,
+            }}
+          />
+        )}
+      </GradientOrColorContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 400,
+    margin: 30,
+    marginTop: 20,
+    borderRadius: 20,
+  },
+});
 
 export default DetailPokemonScreen;
